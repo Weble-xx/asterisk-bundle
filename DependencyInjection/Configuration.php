53617939b4a17bcf6da615048911876b577a9683
Markus Fohrer <weble@me.com>
2024-12-10 11:41:54 +0100

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -13,8 +13,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('asterisk');
+        $treeBuilder = new TreeBuilder('asterisk');
+        $rootNode = $treeBuilder->getRootNode();
 
         $this->addConnections($rootNode);
         $this->addClients($rootNode);
